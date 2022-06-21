78330d2c2d399b9bb49c3f7a26ce20529808b695
zhaojie <1979170837@qq,com>
2022-06-21 21:39:56 +0800

    消费者demo

---
+++ kafka-demo/src/main/java/org/apache/kafka/demo/consumer/ConsumerDemo.java
@@ -0,0 +1,39 @@
+package org.apache.kafka.demo.consumer;
+
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * @author Jie Zhao
+ * @date 2022/6/21 21:24
+ */
+public class ConsumerDemo {
+
+    public static void main(String[] args) {
+
+        Properties props = new Properties();
+
+        props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "test_group");
+        Consumer<String, String> consumer = new KafkaConsumer<>(props);
+
+        List<String> topics = new ArrayList<>();
+        topics.add("quickstart-events");
+        consumer.subscribe(topics);
+
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(1));
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.println(record);
+            }
+        }
+    }
+}
